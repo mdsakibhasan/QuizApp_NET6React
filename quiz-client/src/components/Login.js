52588ac8f8fd99644291a8eed4ec0bcd38ac8e38
Md.Shakhaoat hossain Sakib <md.shakhaoathossain@gmail.com>
2022-09-12 15:16:52 +0600

    UI done for Quiz App
    UI done for Quiz App

---
+++ quiz-client/src/components/Login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import Center from './Center'
+
+
+export default function Login() {
+  return (
+    <Center>
+      <Card sx={{width:400}}>
+      <CardContent sx={{textAlign:"center"}}>
+        <Typography variant="h3" sx={{my:3}}>
+          Quiz App
+        </Typography>
+        <Box sx={{
+          '& .MuiTextField-root':{
+            m:1,
+            width:'90%'
+          }
+        }}>
+        <form noValidate autoComplete="off">
+            <TextField
+              label="Email"
+              name="email"
+              variant="outlined" />
+            <TextField
+              label="Name"
+              name="name"
+              variant="outlined" />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{width:'90%'}}>Start</Button>
+        </form>
+      </Box>
+      </CardContent>
+    </Card>
+    </Center>
+    
+  )
+}
